f251db65c07da2136651f24b2450fd06f5e2834d
yesthecarlos <carlosmendez86@gmail.com>
2021-04-23 17:02:34 -0700

    WIP: switching currencies

--- src/main.js
+++ src/main.js
@@ -8,11 +8,25 @@ function clear(){
   $("#result").empty();
 }
 
-function buildPage(response){
+function currencyChoice(response) {
+  let converted = 0
+  if ($("#currency") === "USD"){
+    converted = response.conversion_rates.USD.val();
+  } else if ($("#currency") === "EUR") {
+    converted = response.conversion_rates.EUR;
+  } else if ($("#currency") === "JPY") {
+    converted = response.conversion_rates.JPY;
+  } else if ($("#currency") === "MXN") {
+    converted = response.conversion_rates.MXN;
+    }
+    return converted;
+  }
+
+function buildPage(response, converted){
   if (response){
+    currencyChoice();
     let input = $("#dollarAmount").val();
-    let currency = $("#currency").val();
-    $("#result").append(input * response.conversion_rates.currency);
+    $("#result").append(input * converted);
   } else {
     $("#errors").text(`There was an error: ${response.message}`);
   }
@@ -25,6 +39,6 @@ $(document).ready(function() {
     Exchanger.exchanger()
       .then(function(response){
         buildPage(response);
-      })
-  })
-})
+      });
+  });
+});
